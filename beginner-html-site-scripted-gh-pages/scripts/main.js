577c69d5a14d85e3926480dd22b25ba79825a1c7
Brad Gardiner <bradgardiner21@gmail.com>
2017-11-19 18:58:58 -0500

    Add files via upload
    Adding node.js and updated UI

---
+++ beginner-html-site-scripted-gh-pages/scripts/main.js
@@ -0,0 +1,42 @@
+// Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+	} else {
+	  myImage.setAttribute ('src','images/firefox-icon.png');
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+function sync(){
+
+	var n1 = document.getElementById('n1');
+ 	var n2 = document.getElementById('n2');
+	alert(n1.value);
+ 	n2.value = n1.value;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+	alert("Hello");
+}
