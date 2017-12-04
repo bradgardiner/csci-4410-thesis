9d8814fddd8d87e3c986858b7ab36bb5ce6208fa
Brad Gardiner <bradgardiner21@gmail.com>
2017-12-04 00:40:22 -0500

    improving on node

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
-var st = require('st');
-var path = require('path');
+// var st = require('st');
+// var path = require('path');
 
 
 
@@ -21,6 +21,17 @@ app.use(function(request, response, next) {
 app.get('/', function(request, response) {
 	response.sendFile(__dirname + '/index.html');
 });
+
+app.get('/styles/style.css', function(request, response) {
+	response.sendFile(__dirname + '/styles/style.css');
+});
+
+app.get('/src/ace.js', function(request, response) {
+	response.sendFile(__dirname + '/src/ace.js');
+});
+
+
+
 
 app.listen(app.get('port'), function() {
 	console.log('Node/Express listening on port ' + app.get('port'));
